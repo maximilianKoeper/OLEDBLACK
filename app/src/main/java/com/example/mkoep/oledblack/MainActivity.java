4fd263b9b9b5e0c2b5020aa92a5e352f9126cc24
maximilianKoeper <m.koeper@gmx.net>
2017-08-31 19:07:19 +0200

    Add Switch to enable overlay

--- app/src/main/java/com/example/mkoep/oledblack/MainActivity.java
+++ app/src/main/java/com/example/mkoep/oledblack/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.mkoep.oledblack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch switchButton, switchButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchButton = (Switch) findViewById(R.id.switch1);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
+                if (bChecked) {
+                    //textView.setText(switchOn);
+                    startOverlay();
+                } else {
+                    //textView.setText(switchOff);
+                }
+            }
+        });
+
     }
+
+    public void startOverlay(){
+        Intent svc = new Intent(this, OverlayShowingService.class);
+        startService(svc);
+    }
+
 }
