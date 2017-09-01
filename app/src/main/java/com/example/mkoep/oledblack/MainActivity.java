1bdbeaabf9b0447e272e6c70e5933c8eeb809760
maximilianKoeper <m.koeper@gmx.net>
2017-09-01 11:33:08 +0200

    Added Menu and Reset

--- app/src/main/java/com/example/mkoep/oledblack/MainActivity.java
+++ app/src/main/java/com/example/mkoep/oledblack/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -69,6 +70,25 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_reset) {
+            OverlayShowingService.col = 0xff000000;
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void setColor(){
